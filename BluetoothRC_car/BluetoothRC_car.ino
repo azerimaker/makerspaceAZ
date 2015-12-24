d8780156bf88906c748cb7a2258dd83a9c0b0a21
condorxan <orxan_amiraslanov@yahoo.com>
2015-12-25 00:30:41 +0100

    az kommentler elavesi

--- BluetoothRC_car/BluetoothRC_car.ino
+++ BluetoothRC_car/BluetoothRC_car.ino
@@ -1,42 +1,44 @@
 /*
  * www.Makerspace-Az.org
- * Sketch: 4X4 Robot car controller with Smartphone over Bluetooth
- * 
- * modified 24 December 2015
+ * Arduino Sketch: 4X4 Robot mashin kontroller
+ * Kommunikasiya: Bluetooth (BT)
+ * Android App: Bluetooth RC
+ * last modified: 24 December 2015
  * by (azerimaker)
 */
 
 #include <SoftwareSerial.h>
 
-#define Rx 2          // Connect to Tx pin on BT module        
-#define Tx 3          // Connect through Resitive divider to Rx pin of BT module 
+#define Rx 2          // BT modulun TX pinine qoshulur     
+#define Tx 3          // Gerginlik bolucu rezistorlarla BT modulun RX pinine qoshulur
 #define motor1A 11    // INA
 #define motor1B 10    // INB
 #define motor2A 9     // INC
 #define motor2B 6     // IND
 
-// variables and constants
+// vdeyishenlar ve sabitler
 char BT_RECEIVED;  
  
 volatile unsigned char SPEED_MIX = 0;
-volatile unsigned char SET_SPEED = 100; // default 100
-boolean FORWARD = false;
-boolean BACKWARD = true;
+volatile unsigned char SET_SPEED = 100; // bashlangic suret 100 
+boolean FORWARD = false; // ireli
+boolean BACKWARD = true; // geri
   
-// function prototypes
-char BT_read(void);
-void driveRobot(char COMMAND);
+// funksiya prototipleri, funksiyalar ozleri ise loop() funksiyasindan sonra yazilib
+
+char BT_read(void);				// BT-dan gelen simvolu oxuyur
+void driveRobot(char COMMAND);  // Bu simvola uyghun robotu idare edir
 void setSpeedRight(unsigned char SPEED, boolean DIR);
 void setSpeedLeft(unsigned char SPEED, boolean DIR);
-void brakeMotor(void);
+void brakeMotor(void);			// tekerleri tormozlayir
 
-// define software serial port
+// Hardware serial PC ile kommunikasiya ucun lazim oldughu ucun SoftwareSerial ishledirik
 SoftwareSerial BTserial(Rx, Tx);
              
-                                   
+// bu funksiya cemi bir defe program ishe dushende icra olunur                                   
 void setup() 
 {
-
+// pinleri girish/cixish kimi teyin et
   pinMode(Rx, INPUT);
   pinMode(Tx, OUTPUT);
   pinMode(motor1A, OUTPUT);
@@ -44,35 +46,35 @@ void setup()
   pinMode(motor2A, OUTPUT);
   pinMode(motor2B, OUTPUT);
   
-  brakeMotor(); // set pins to 0
+  brakeMotor(); // tekerleri tormozla, ehtiyat ucun
   
   Serial.begin(9600);
-  BTserial.begin(9600); // SoftwareSerial baud rate for JY-MCU v1.03 defaults to 9600.
-  
+  BTserial.begin(9600); // SoftwareSerial baud qiymeti HC-06 BT modulu ucun 9600.
+  						// AT+BAUDx emri ile deyishe bilersiniz, etrafli meqalede
   delay(50);
   
   Serial.println("Type AT commands!");
  
-  BTserial.print("AT"); // send status command
-  //BTserial.print("AT\r\n"); // USE THIS FOR HC-05
+  BTserial.print("AT"); // sstatus oyrenme emrini BT modula gonder
+  //BTserial.print("AT\r\n"); // HC-05 modullari ucun bu emrden istifade edin
   while(BTserial.available()){ 
-      Serial.write(BTserial.read()); // print back the response
+      Serial.write(BTserial.read()); // sorgunun cavabini PC-de print et
     }
   delay(50);
 } 
 
-
+// Bu funksiya sonsuz olaraq tekrarlanir. 
 void loop(){
 
    if (Serial.available()){
-     BTserial.write(Serial.read());  // Read user input if available send it to BT.
+     BTserial.write(Serial.read());  // Eger PC-den nese gonderirikse, simvollari BT modula gonder
    }
        
    if(BTserial.available()){
      while(BTserial.available()){
-        BT_RECEIVED = BT_read();   // read BT data   
-        Serial.write(BT_RECEIVED); // Read BT module if available.
-        driveRobot(BT_RECEIVED);   // enable command
+        BT_RECEIVED = BT_read();   // BT-dan gelen simvollari oxu
+        Serial.write(BT_RECEIVED); // PC-e gonder
+        driveRobot(BT_RECEIVED);   // Simvolu hemcinin Robotu idare funksiyasina gonder
      }
       
     }
@@ -82,14 +84,15 @@ void loop(){
 
 char BT_read(void)
 {
-    char c = (char)BTserial.read();
+    char c = (char)BTserial.read();  // BT-dan gelen simvollari oxuyur
     return c;
 }
 
 
 void driveRobot(char COMMAND){
   
-  SPEED_MIX = map(SET_SPEED, 0, 255, 0, 127);
+  SPEED_MIX = map(SET_SPEED, 0, 255, 0, 127); // cuzi donmeler aparmaq ucun esas suret deyishenini
+  											  // texminen 2 defe azalt ve uygun tekerin yeni suretini teyin et
   
   switch(COMMAND){
         case '0':  
@@ -166,6 +169,7 @@ void driveRobot(char COMMAND){
     }
 }
 
+// sag tekerlerin suretini ve istiqametini teyin eden funksiya
 void setSpeedRight(unsigned char SPEED, boolean DIR)
 {
   if(DIR == true){
@@ -180,6 +184,8 @@ void setSpeedRight(unsigned char SPEED, boolean DIR)
   
 }
 
+
+// sol tekerlerin suretini ve istiqametini teyin eden funksiya
 void setSpeedLeft(unsigned char SPEED, boolean DIR)
 {
   if(DIR == true){
